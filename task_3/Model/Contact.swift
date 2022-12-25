8d84b4d5abd114d3f831cae9185144b436810327
Artem Sulzhenko <artiklol2012@gmail.com>
2022-12-25 22:22:22 +0300

    TEST

--- task_3/Model/Contact.swift
+++ task_3/Model/Contact.swift
@@ -7,33 +7,4 @@
 
 import Foundation
 
-struct Contact: Codable {
-    var name: String
-    var surname: String
-    var phoneNumber: String
-    let image: Data?
-    var favorite: Bool
 
-    var encoded: Data? {
-        let encoder = PropertyListEncoder()
-        return try? encoder.encode(self)
-    }
-
-    init?(from data: Data) {
-        let decoder = PropertyListDecoder()
-        guard let contact = try? decoder.decode(Contact.self, from: data) else { return nil }
-        name = contact.name
-        surname = contact.surname
-        phoneNumber = contact.phoneNumber
-        image = contact.image
-        favorite = contact.favorite
-    }
-
-    init(name: String, surname: String, phoneNumber: String, image: Data?, favorite: Bool) {
-        self.name = name
-        self.surname = surname
-        self.phoneNumber = phoneNumber
-        self.image = image
-        self.favorite = favorite
-    }
-}
--- task_3/Support Localization/String+Ex.swift
+++ task_3/Support Localization/String+Ex.swift
@@ -5,16 +5,4 @@
 //  Created by Artem Sulzhenko on 25.12.2022.
 //
 
-import Foundation
 
-extension String {
-    func localized() -> String {
-        NSLocalizedString(
-            self,
-            tableName: "Localizable",
-            bundle: .main,
-            value: self,
-            comment: self
-        )
-    }
-}
