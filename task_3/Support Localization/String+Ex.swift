b0b231eb7d2e6a59a6d1f03dbec553b927af40b1
Artem Sulzhenko <artiklol2012@gmail.com>
2022-12-25 22:26:28 +0300

    FINISHED task

--- task_3/Support Localization/String+Ex.swift
+++ task_3/Support Localization/String+Ex.swift
@@ -5,4 +5,16 @@
 //  Created by Artem Sulzhenko on 25.12.2022.
 //
 
+import Foundation
 
+extension String {
+    func localized() -> String {
+        NSLocalizedString(
+            self,
+            tableName: "Localizable",
+            bundle: .main,
+            value: self,
+            comment: self
+        )
+    }
+}
